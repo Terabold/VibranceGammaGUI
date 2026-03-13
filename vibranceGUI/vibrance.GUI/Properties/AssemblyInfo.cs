72b0f380a70a9339e3fb2d20d48593d482925490
Terbold <128602033+Terabold@users.noreply.github.com>
2026-03-13 04:23:28 +0200

    Initial Release: VibranceGammaGUI v2.0.0 (Vibe Coded)

---
+++ vibranceGUI/vibrance.GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General information about an assembly is controlled through the following
+// attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VibranceGammaGUI")]
+[assembly: AssemblyDescription("Advanced Hardware-Level NVIDIA Vibrance and Gamma Control")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Terabold")]
+[assembly: AssemblyProduct("VibranceGammaGUI")]
+[assembly: AssemblyCopyright("Copyright © 2026 Terabold")]
+[assembly: AssemblyTrademark("NVIDIA is a trademark of NVIDIA Corporation")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
+// COM, set the ComVisible attribute for that type to true.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8349f303-6b2b-4645-8f17-19731ea916ab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or use the default Build and Revision numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
